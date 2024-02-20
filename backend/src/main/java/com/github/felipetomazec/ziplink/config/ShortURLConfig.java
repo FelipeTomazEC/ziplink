06f83b88b4e85a88b25f7f13b7bffa5f77bc00a1
felipetomazec <felipetomaz2206@gmail.com>
2024-02-20 20:41:51 -0300

    fix: add missing constructor

--- backend/src/main/java/com/github/felipetomazec/ziplink/config/ShortURLConfig.java
+++ backend/src/main/java/com/github/felipetomazec/ziplink/config/ShortURLConfig.java
@@ -2,10 +2,12 @@ package com.github.felipetomazec.ziplink.config;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Configuration
 @ConfigurationProperties(prefix = "ziplink.short-url-config")
